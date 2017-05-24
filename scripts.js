6719fef194fb9e73336f4913c427cc39b40511f1
Cong Bom <advcong@gmail.com>
2017-05-24 18:32:40 +0700

    Vuejs added

--- scripts.js
+++ scripts.js
@@ -1,19 +1,40 @@
-Vue.component('pokemon', {
-	template: "<div class='pokemon'>Select Pokemon</div>"
-});
-
-Vue.component('ivnumber', {
-	template: "<div class='pokemon'>Select ivnumber</div>"
-});
-
-Vue.component('results', {
-	template: "<div class='pokemon'>CP results</div>"
-});
-
-Vue.component('levels', {
-	template: "<div class='pokemon'>Select Levels</div>"
-});
-
 new Vue({
-	el: '#app'
-})
+	el: '#app',
+	data: {
+		pokemons: null,
+		scalar: null,
+		pkm: 001,
+		atk: 15,
+		def: 15,
+		stm: 15,
+		level: 20,
+	},
+	created: function() {
+		/* Stats */
+		this.$http.get('stats.json').then(response => {
+	    	this.pokemons = response.body;
+	  	}, response => {
+	    	alert('Stats Error!');
+	  	});
+
+		/* Scalar */
+		this.$http.get('scalar.json').then(response => {
+	    	this.scalar = response.body;
+	  	}, response => {
+	    	alert('Scalar Error!');
+	  	});
+	},
+	computed: {
+		result_cp: function() {
+			return this.atk * this.def * this.stm * this.level;
+		},
+		result_hp: function() {
+			return this.stm * this.level;
+		}
+	},
+	watch: {
+		pkm: function( val ) {
+			alert( val );
+		}
+	}
+});
