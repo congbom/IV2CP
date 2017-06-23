43192ba04fda47ccd34b25c62591b8c7e81436dd
Cong Bom <advcong@gmail.com>
2017-06-23 10:19:26 +0700

    Add Dust and Candy require

--- scripts.js
+++ scripts.js
@@ -2,8 +2,10 @@ new Vue({
 	el: '#app',
 	data: {
 		pokemons: null,
-		currnet_pkm: "242",
+		currnet_pkm: 3,
 		scalars: null,
+		candies: null,
+		dusts: null,
 		bonus_atk: 15,
 		bonus_def: 15,
 		bonus_sta: 15,
@@ -19,9 +21,23 @@ new Vue({
 
 		/* Scalar */
 		this.$http.get('scalar.json').then(response => {
-	    	this.scalars = response.body;
+	    	this.scalars = response.body;	    	
 	  	}, response => {
 	    	alert('Scalar Error!');
+	  	});
+
+		/* Candy */
+		this.$http.get('scandy.json').then(response => {
+	    	this.candies = response.body;
+	  	}, response => {
+	    	alert('Candy Error!');
+	  	});
+
+		/* Dust */
+		this.$http.get('sdust.json').then(response => {
+	    	this.dusts = response.body;
+	  	}, response => {
+	    	alert('Dust Error!');
 	  	});
 	},
 	computed: {
@@ -32,24 +48,32 @@ new Vue({
 
 				return pokemons[currnet_pkm];				
 			}
+
+			return 0;
 		},
 		base_atk: function() {
 			if ( this.stats ) {
 				var stats = this.stats;
 				return Number( stats.atk );
 			}
+
+			return 0;
 		},
 		base_def: function() {
 			if ( this.stats ) {
 				var stats = this.stats;
 				return Number( stats.def );
 			}
+
+			return 0;
 		},
 		base_sta: function() {
 			if ( this.stats ) {
 				var stats = this.stats;
 				return Number( stats.sta );
 			}
+
+			return 0;
 		},
 		iv_percent: function() {
 			var percent = ( Number(this.bonus_atk) + Number(this.bonus_def) + Number(this.bonus_sta) ) * 100 / 45;
@@ -60,8 +84,12 @@ new Vue({
 				var level = this.level;
 				var scalars = this.scalars;
 
-				return Number( scalars[level] );				
+				if ( level > 39 || level < 1 ) return 0;
+		        if ( level % 1 ) return Math.pow( ( Math.pow( scalars[ level-0.5 ], 2) + Math.pow( scalars[ level-1.5 ], 2) ) / 2, 0.5);
+		        return scalars[ level-1];
 			}
+
+			return 0;
 		},
 		result_cp: function() {
 			if ( this.cp_scalar ) {
@@ -72,11 +100,39 @@ new Vue({
 
 				return Math.floor( atk * def * sta * oth );
 			}
+
+			return 0;
 		},
 		result_hp: function() {
 			if ( this.cp_scalar ) {
 				return Math.floor( ( this.base_sta + Number(this.bonus_sta) ) * this.cp_scalar );
 			}
+
+			return 0;
+		},
+		dust: function() {
+			if ( this.level && this.dusts ) {
+				var level = this.level;
+				var dusts = this.dusts;
+
+				if ( level > 39 || level < 1 ) return 0;
+				if ( level % 1 ) return dusts[ level - 1.5 ];
+				return dusts[ level - 1 ];
+			}
+
+			return 0;
+		},
+		candy: function() {
+			if ( this.level && this.candies ) {
+				var level = this.level;
+				var candies = this.candies;
+
+				if ( level > 39 || level < 1 ) return 0;
+				if ( level % 1 ) return candies[ level - 1.5 ];
+				return candies[ level - 1 ];
+			}
+
+			return 0;
 		}
 	}
 });
