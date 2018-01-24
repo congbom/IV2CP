c10bcf1b1cf16cc295f8734ddf265b04896b4007
Cong Bom <advcong@gmail.com>
2018-01-24 16:04:33 +0700

    Update Kyogre stats

--- scripts.js
+++ scripts.js
@@ -17,7 +17,7 @@ new Vue({
 		/* Stats */
 		this.$http.get('stats.json').then(response => {
 	    	this.pokemons = response.body;
-	    	this.selected = this.pokemons[382];
+	    	this.selected = this.pokemons[381];
 	  	}, response => {
 	    	alert('Stats Error!');
 	  	});
