7e2e57c729f8f8e569c272679fc2f68752cc3380
Cong Bom <advcong@gmail.com>
2018-03-17 11:20:35 +0700

    Lugia default

--- scripts.js
+++ scripts.js
@@ -17,7 +17,7 @@ new Vue({
 		/* Stats */
 		this.$http.get('stats.json').then(response => {
 	    	this.pokemons = response.body;
-	    	this.selected = this.pokemons[381];
+	    	this.selected = this.pokemons[248];
 	  	}, response => {
 	    	alert('Stats Error!');
 	  	});
